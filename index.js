ce9dd8149091cdbcae44de7918ceda65f9fc6b42
Jess Guttenberg <jessigu@gmail.com>
2018-08-03 16:55:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your functions and code here
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  kittens.push('Ralph')
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift('Bob')
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+var appendKitten = kittens.splice('Broom')
+return kittens
+}
+
+function prependKitten(name) {
+var prependKitten = kittens.splice(0, 0, 'Arnold')
+return kittens.concat(prependKitten)
+}
+
+function removeLastKitten() {
+  var removeLastKitten = kittens.slice(0, 2)
+  return removeLastKitten
+}
+
+function removeFirstKitten() {
+  var removeFirstKitten = kittens.slice(1)
+  return removeFirstKitten
+}
